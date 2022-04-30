3cd25d60e02d783f2aad5cdff3c3fd903bf8e744
DianaKosha <55238176+DianaKosha@users.noreply.github.com>
2022-04-30 17:19:56 +0300

    Create pp.py

---
+++ pp.py
@@ -0,0 +1,27 @@
+from pygame import *
+
+class GameSprite(srpite.Sprite):
+  def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+    super().__init__()
+    self.image = transform.scale(image.load(player_image))
+    self.speed = player_speed
+    self.rect = self.image.get_rect() #rectangle - прямокутник
+    self.rect.x = player_x
+    self.rect.y = player_y
+  def reset(self):
+    window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+  def update_right(self):
+    keys = key.get_pressed()
+    if keys[K_UP] and self.rect.y > 5:
+      self.rect.y -= self.speed
+    if keys[K_DOWN] and self.rect.y < 420:
+      self.rect.y += self.speed
+  def update_left(self):
+    pass
+  
+  
+  
+  win_width = 600
+  win_height = 500
