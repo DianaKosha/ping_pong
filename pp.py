f63c16005b8569303ab5e5228433891cb76913dc
DianaKosha <55238176+DianaKosha@users.noreply.github.com>
2022-05-07 13:31:18 +0300

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,27 +1,67 @@
 from pygame import *
-
+ 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
-        super().__init__()
-        self.image = transform.scale(image.load(player_image))
-        self.speed = player_speed
-        self.rect = self.image.get_rect() #rectangle - прямокутник
-        self.rect.x = player_x
-        self.rect.y = player_y
-    def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (wight, height))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+ 
+
 
 class Player(GameSprite):
-    def update_right(self):
-        keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
-            self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 420:
-            self.rect.y += self.speed
-    def update_left(self):
-        pass
-    
-  
-  
+   def update_r(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+   def update_l(self):
+       keys = key.get_pressed()
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+ 
+
+
+back = (200, 255, 255)
 win_width = 600
 win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+ 
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+ 
+
+  
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+ 
+
+
+speed_x = 3
+speed_y = 3
+ 
+
+
+while game:
+   for e in event.get():
+       if e.type == QUIT:
+           game = False
+
+ 
+   display.update()
+   clock.tick(FPS)
+
+
